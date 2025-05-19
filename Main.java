35087e6601766b024ee1ae573393954929bf89ee
Paolo Benincasa <paolobenincasa1984@gmail.com>
2025-05-19 16:29:58 +0200

    switch sintassi diversa

--- Main.java
+++ Main.java
@@ -1,26 +1,34 @@
 package test;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		// Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("inserisci il tuo nome");
-		String nome = scanner.nextLine();
+		// System.out.println("inserisci il tuo nome");
+		// String nome = scanner.nextLine();
 		
-		System.out.println("inserisci il tuo cognome");
-		String cognome = scanner.nextLine();
+		// System.out.println("inserisci il tuo cognome");
+		// String cognome = scanner.nextLine();
 		
-		System.out.println("quanti anni hai?");
-		int eta = scanner.nextInt();
-		scanner.nextLine();
+		// System.out.println("quanti anni hai?");
+		// int eta = scanner.nextInt();
+		// scanner.nextLine();
 		
-		System.out.println("dove abiti?");
-		String citta = scanner.nextLine();
+		// System.out.println("dove abiti?");
+		// String citta = scanner.nextLine();
 		
-		System.out.println("ciao, " + nome + " " + cognome + ", hai " + eta + " anni e abiti a " + citta);
+		// System.out.println("ciao, " + nome + " " + cognome + ", hai " + eta + " anni e abiti a " + citta);
+
+		String day = "Sunday";
+		String typeOfDay = switch (day){
+			case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> "Weekday";
+			case "Saturday", "Sunday"-> "Weekend";
+			default -> "unknown day";
+		};
+		System.out.println(typeOfDay);
+
 	}
+
 
 }
